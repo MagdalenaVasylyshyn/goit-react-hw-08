f9c2fb93f03ae5cf2d90b2927359bf4a19c71d09
MagdalenaVasylyshyn <141566623+MagdalenaVasylyshyn@users.noreply.github.com>
2024-04-02 18:04:58 +0300

    Fourth version of eighth homework

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,8 +1,8 @@
 import { selectContacts } from "../contacts/selectors";
 import { createSelector } from "@reduxjs/toolkit";
-export const selectFilter = (state) => state.filters.values;
+export const selectFilter = state => state.filters.name;
 export const selectFilteredContacts = createSelector(
-  [selectContacts, selectNameFilter],
+  [selectContacts, selectFilter],
   (contacts, filter) => {
     {
       return contacts.length > 0
